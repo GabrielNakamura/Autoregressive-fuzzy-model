1253f6be91cadc33f58527d29610e75bd1b17f8b
Gabriel Nakamura <gabriel.nakamura.souza@gmail.com>
2020-09-06 12:22:07 -0300

    adjusting parameters to calculate type I error and power

--- R/D_typeI_power_analysis.R
+++ R/D_typeI_power_analysis.R
@@ -2,7 +2,7 @@
 
 # data, functions and libraries -------------------------------------------
 library(parallel)
-source(here::here("functions", "autoregressive_GN_01-09.R"))
+source(here::here("R", "functions", "autoregressive_GN_01-09.R"))
 
 
 # power - 1 envir TRUE ----------------------------------------------------
@@ -10,9 +10,9 @@ source(here::here("functions", "autoregressive_GN_01-09.R"))
 runs<- 1:1000 # number of tests
 Nspp<- 50 
 Ncomm<- 50
-power<- 1 # grafen parameter
+power<- 0.8 # grafen parameter
 perm<- 1000 # number of permutations in taxa shuffle null model
-envir<- TRUE # environment
+envir<- FALSE # environment
 
 tree_list<- lapply(runs, function(x){
   geiger::sim.bdtree(b=0.1,d=0,stop="taxa",n=Nspp,t=30,extinct=FALSE)
@@ -20,20 +20,20 @@ tree_list<- lapply(runs, function(x){
 
 ncor<- detectCores() # setting number of cores
 CL<- makeCluster(ncor, setup_timeout = 0.5) 
-clusterExport(CL,c("simul_metacomm_autoregress", "Nspp", "Ncomm", "power", "envir"))
+clusterExport(CL,c("simul_metacomm_autoregress", "Nspp", "Ncomm", "power", "envir", "perm"))
 clusterEvalQ(CL,library(vegan))
 clusterEvalQ(CL,library(ape))
 clusterEvalQ(CL,library(SYNCSA))
 clusterEvalQ(CL,library(PCPS))
 clusterEvalQ(CL,library(QuantPsyc))
 
-res_rho1_envir<- parLapply(cl = CL, X = tree_list, fun = function(i) 
+system.time(res_rho.8_envir<- parLapply(cl = CL, X = tree_list, fun = function(i) 
   simul_metacomm_autoregress(tree = i, Nspp = Nspp, 
-                             Ncomm = Ncomm, perm = 10, power = 1, u = 5, 
+                             Ncomm = Ncomm, perm = perm, power = power, u = 5, 
                              envir = envir, binary = TRUE, test = TRUE)
-  ) # run analysis
+  )) # run analysis
 stopCluster(CL)
-
+saveRDS(res_rho.8_envir, file = here::here("output", "Result_rho08_noEnv.rds"))
 
 # power 0.0001 envir TRUE -------------------------------------------------
 
@@ -41,24 +41,24 @@ library(parallel)
 runs<- 1:1000
 Nspp<- 50
 Ncomm<- 50
-power<- 0.0001
+power<- 1.2
 perm<- 1000
-envir<- TRUE
+envir<- FALSE
 tree_list<- lapply(runs, function(x){
   geiger::sim.bdtree(b=0.1,d=0,stop="taxa",n=Nspp,t=30,extinct=FALSE)
 })
 ncor<- detectCores()
 CL<- makeCluster(ncor, setup_timeout = 0.5)
-clusterExport(CL,c("simul_metacomm_autoregress", "Nspp", "Ncomm", "power", "envir"))
+clusterExport(CL,c("simul_metacomm_autoregress", "Nspp", "Ncomm", "power", "envir", "perm"))
 clusterEvalQ(CL,library(vegan))
 clusterEvalQ(CL,library(ape))
 clusterEvalQ(CL,library(SYNCSA))
 clusterEvalQ(CL,library(PCPS))
 clusterEvalQ(CL,library(QuantPsyc))
 
-res_rho1_envir<- parLapply(cl = CL, X = tree_list, fun = function(i) 
+res_rho1.2_envir<- parLapply(cl = CL, X = tree_list, fun = function(i) 
   simul_metacomm_autoregress(tree = i, Nspp = Nspp, 
-                             Ncomm = Ncomm, perm = 10, power = 1, u = 5, 
+                             Ncomm = Ncomm, perm = perm, power = power, u = 5, 
                              envir = envir, binary = TRUE, test = TRUE)
 )
 stopCluster(CL)
